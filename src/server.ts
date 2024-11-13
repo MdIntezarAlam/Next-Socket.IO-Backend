b6565225609596aedc3cf7cbda3e6fd75b9d0654
Md Intezar Alam <mdintezara35@gmail.com>
2024-11-13 14:16:54 +0530

    origin allowed

--- src/server.ts
+++ src/server.ts
@@ -3,32 +3,46 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { socketControllers } from "./controller/socketControllers";
 import connectDB from "./config/db";
-const PORT = process.env.PORT || 5000;
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 
 dotenv.config()
 connectDB()
+const app = express();
 
 
 const allowOrigin = [
     "https://dev-chatme.netlify.app/",
     "https://chatme-qulw.onrender.com/"
 ]
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        console.log("Incoming origin:", origin); // to check incoming origin
+        if (allowOrigin.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-const app = express();
+
+const server = createServer(app);
+const io = new Server(server, { cors: corsOptions });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions))
 
-const httpServer = createServer(app);
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: allowOrigin
-    },
-});
+socketControllers(io)
 
-socketControllers(io);
-httpServer.listen(PORT, () => {
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
     console.log(`server is working on ${PORT}`);
 });
